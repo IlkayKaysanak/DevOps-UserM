46fd55d397d803a68520467e672596c839b1f480
İlkay Kaysanak <ikaysanak@hotmail.com>
2023-10-12 15:39:29 +0300

    mysql

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,6 +1,3 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
 CREATE DATABASE IF NOT EXISTS test;
 
 USE test;
@@ -12,6 +9,3 @@ CREATE TABLE IF NOT EXISTS user (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
-
-INSERT INTO user (name, surname, email, password) VALUES
-('ilkay', 'kaysanak', 'ikaysanak@gmail.com','111111');
